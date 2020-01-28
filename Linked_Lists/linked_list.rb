112f159af95039a8444fc97e5fa36ff3fa99e254
gwnm <gwnmagee@gmail.com>
2020-01-28 21:00:37 +0000

    First draft

---
+++ Linked_Lists/linked_list.rb
@@ -0,0 +1,287 @@
+class Node
+
+	attr_accessor :value, :next_node
+
+	def initialize value = nil, next_node = nil
+
+		@value = value
+
+		@next_node = next_node
+
+	end
+
+end
+
+
+
+class LinkedList
+
+	def initialize
+
+		@head = Node.new("head",nil)
+
+	end
+
+	# adds a new node to the end of the list
+
+	def append(value)
+
+		self.tail.next_node = Node.new(value,nil)
+
+	end
+
+	# adds a new node to the start of the list
+
+	def prepend(value)
+
+		@head.next_node = Node.new(value,@head.next_node)
+
+	end
+
+	# returns the total number of nodes in the list
+
+	def size
+
+		if @head.next_node
+
+			node = @head.next_node
+
+			i = 1
+
+			while node.next_node
+
+				node = node.next_node
+
+				i += 1
+
+			end
+
+			return i
+
+		else
+
+			return 0
+
+		end
+
+	end
+
+	# returns the first node in teh list
+
+	def head
+
+		@head
+
+	end
+
+	# returns the last node in the list
+
+	def tail
+
+		if @head.next_node
+
+			node = @head.next_node
+
+			while node.next_node
+
+				node = node.next_node
+
+			end
+
+			node
+
+		else
+
+			@head
+
+		end
+
+	end
+
+	# removes the last element from the list (and returns it)
+
+	def pop
+
+		if @head.next_node
+
+			node = @head.next_node
+
+			while node.next_node.next_node
+
+				node =  node.next_node
+
+			end
+
+			popping = node.next_node
+
+			node.next_node = nil
+
+			return popping
+
+		else
+
+			return nil
+
+		end
+
+	end
+
+	# returns true if the passed in value 
+
+	# is in the list and otherwise returns false.
+
+	def contains?(value)
+
+		if @head.next_node
+
+			node = @head.next_node
+
+			while node.next_node
+
+				if node.value == value
+
+					return true
+
+				else
+
+					node = node.next_node
+
+				end
+
+			end
+
+			return false
+
+		else
+
+			return false
+
+		end
+
+	end
+
+	# returns the index of the node containing data, or nil if not found.
+
+	def find(value)
+
+		if @head.next_node
+
+			node = @head.next_node
+
+			i = 0
+
+			while node.next_node
+
+				if node.value == value
+
+					return i
+
+				else
+
+					node = node.next_node
+
+					i += 1
+
+				end
+
+			end
+
+			return nil
+
+		else
+
+			return nil
+
+		end
+
+	end
+
+	# represent your LinkedList objects as strings, 
+
+	# so you can print them out and preview them in the console. 
+
+	# The format should be: ( data ) -> ( data ) -> ( data ) -> nil
+
+	def to_s
+
+		if @head.next_node
+
+			s = "(#{@head.next_node.value}) -> "
+
+			node = @head.next_node
+
+			while node.next_node
+
+				s += "(#{node.next_node.value}) -> "
+
+				node = node.next_node
+
+			end
+
+			s += "nil"
+
+			return s
+
+		else
+
+			return "nil"
+
+		end
+
+	end
+
+	# returns the node at a given index
+
+	def at(index)
+
+		if self.size > index
+
+			if @head.next_node
+
+				node = @head.next_node
+
+				i = 0
+
+				until i == index
+
+					node = node.next_node
+
+					i += 1
+
+				end
+
+				return node
+
+			else
+
+				return nil
+
+			end
+
+		end
+
+		return nil
+
+	end
+
+	# that inserts the data after the given index
+
+	def insert_at(index,value)
+
+		self.at(index-1).next_node = Node.new(value,self.at(index-1).next_node)
+
+	end
+
+	# that removes the node at the given index. (You will need to 
+
+	# update the links of your nodes in the list when you remove a node.)
+
+	def remove_at(index)
+
+		self.at(index-1).next_node = self.at(index).next_node
+
+	end
+
+
+
+end
---
+++ Linked_Lists/linked_list_spec.rb
@@ -0,0 +1,327 @@
+require './linked_list'
+
+
+
+describe "Node" do 
+
+	it "initializes correctly defaults" do
+
+		node = Node.new
+
+		expect(node.value).to eql(nil)
+
+		expect(node.next_node).to eql(nil)
+
+	end
+
+
+
+	it "initializes with values" do 
+
+		node = Node.new(3, 7)
+
+		expect(node.value).to eql(3)
+
+		expect(node.next_node).to eql(7)
+
+	end
+
+end
+
+
+
+describe 'linked_list' do
+
+	  	before do
+
+	  	   @list = LinkedList.new
+
+	  	end
+
+	describe 'initialize' do
+
+	  	it 'creates a head node' do
+
+	  	  	expect(@list.head).to be_a(Node)  
+
+	  	end
+
+	end
+
+	describe 'append' do
+
+		before do
+
+		  @list.append(7)
+
+		end
+
+	  	it 'adds a new node' do
+
+	  	  	expect(@list.head.next_node.value).to eql(7)
+
+	  	end
+
+	  	it 'works twice' do
+
+	  	  	@list.append(3)
+
+	  	  	expect(@list.head.next_node.next_node.value).to eql(3) 
+
+	  	end
+
+	end
+
+	describe 'tail' do
+
+	  	it 'returns head if empty' do
+
+	  	  	expect(@list.tail.value).to eql("head")
+
+	  	end
+
+	  	it 'returns last with 1 value' do
+
+	  	  	@list.append(7)
+
+	  	  	expect(@list.tail.value).to eql(7)
+
+	  	end
+
+	  	it 'works with 2' do
+
+	  	  	@list.append(7)
+
+	  	  	@list.append(4)
+
+	  	  	expect(@list.tail.value).to eql(4) 
+
+	  	end
+
+	end
+
+	describe 'prepend' do
+
+	  	before do
+
+	  	  @list.append(7)
+
+	  	end
+
+	  	it 'prepends' do
+
+	  	   @list.prepend(2)
+
+	  	   expect(@list.head.next_node.value).to eql(2) 
+
+	  	end
+
+	end
+
+	describe 'size' do
+
+		it 'returns 0 if empty' do
+
+		  	expect(@list.size).to eql(0)
+
+		end
+
+		it 'returns 1' do
+
+		  	@list.append(3)
+
+		  	expect(@list.size).to eql(1) 
+
+		end
+
+		it 'returns 3' do
+
+			@list.append(3)
+
+			@list.append(4)
+
+			@list.append(6)
+
+			expect(@list.size).to eql(3) 
+
+		end
+
+	end
+
+	describe 'pop' do
+
+	  	before do
+
+	  		@list.append(3)
+
+	  		@list.append(4)
+
+	  		@list.append(6)
+
+	  	end
+
+	  	it 'removes last element' do
+
+	  	  @list.pop
+
+	  	  expect(@list.size).to eql(2) 
+
+	  	end
+
+	  	it 'returns last element' do
+
+	  		expect(@list.pop.value).to eql(6)
+
+	  	end
+
+	end
+
+  	describe 'contains?' do
+
+  	  before do
+
+  	  	@list.append(3)
+
+  	  	@list.append(4)
+
+  	  	@list.append(6)
+
+  	  end
+
+  	  it 'returns true' do
+
+  	     expect(@list.contains?(4)).to eql(true) 
+
+  	  end
+
+  	  it 'returns false' do
+
+  	     expect(@list.contains?(9)).to eql(false)
+
+  	  end
+
+  	end
+
+  	describe 'find' do
+
+		 before do
+
+		 	@list.append(3)
+
+		 	@list.append(4)
+
+		 	@list.append(6)
+
+		 end
+
+		it 'works when true' do
+
+		 	  expect(@list.find(4)).to eql(1)
+
+		end 
+
+		it 'works when false' do
+
+			expect(@list.find(1)).to eql(nil)     
+
+		end	  
+
+  	end
+
+  	describe 'to_s' do
+
+	  	before do
+
+	  		@list.append(3)
+
+	  		@list.append(4)
+
+	  		@list.append(6)
+
+	  	end
+
+	  	it 'works' do
+
+	  	    expect(@list.to_s).to eql("(3) -> (4) -> (6) -> nil")
+
+	  	end  
+
+  	end
+
+  	describe 'at' do
+
+	  	before do
+
+	  		@list.append(3)
+
+	  		@list.append(4)
+
+	  		@list.append(6)
+
+	  	end	
+
+	  	it 'works if index is in range' do
+
+	  		expect(@list.at(2).value).to eql(6) 
+
+	  	end  
+
+	  	it 'returns nil if out of range' do
+
+	  		  expect(@list.at(7)).to eql(nil)   
+
+	  	end	  
+
+  	end
+
+  	describe 'insert at' do
+
+  	  before do
+
+  	  	@list.append(3)
+
+  	  	@list.append(4)
+
+  	  	@list.append(6)
+
+  	  	@list.insert_at(1,77)
+
+  	  end
+
+  	  it 'does something' do
+
+	  	  expect(@list.at(1).value).to eql(77)
+
+  	  end
+
+  	  it 'doesnt delete anything' do
+
+  	    expect(@list.size).to eql(4) 
+
+  	  end
+
+  	end
+
+  	describe 'remove at' do
+
+  	  before do
+
+  	  	@list.append(3)
+
+  	  	@list.append(4)
+
+  	  	@list.append(6)
+
+  	  	@list.remove_at(1)
+
+  	  end
+
+  	  it 'works' do
+
+  	    expect(@list.to_s).to eql("(3) -> (6) -> nil")
+
+  	  end
+
+  	end
+
+end
