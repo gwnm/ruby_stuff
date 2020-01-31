1e1e13b14f519ef32a0a6e1cd48c369184068180
gwnm <gwnmagee@gmail.com>
2020-01-31 13:16:26 +0000

    draft

--- Advanced/enumerable.rb
+++ Advanced/enumerable.rb
@@ -1,169 +1,213 @@
 module Enumerable
 
-    def my_each
-  
-      for i in 0...self.length
-  
-        yield self[i]
-  
-      end
-  
-      return self
-  
+  def my_each
+
+    i = 0
+
+    while i < self.length
+
+      yield(self[i])
+
+      i += 1
+
     end
-  
-  
-  
-    def my_each_with_index
-  
-      for i in 0...self.length
-  
-        yield self[i], i
-  
-      end
-  
-    end
-  
-  
-  
-    def my_select
-  
-      result=[]
-  
-      for i in 0...self.length
-  
-        if yield self[i]
-  
-          result.push(self[i])
-  
-        end
-  
-      end
-  
-      return result
-  
-    end
-  
-  
-  
-    def my_all?(&block)
-  
-      unless block_given?
-  
-        block= lambda {|x| x}
-  
-      end
-  
-      return my_select(&block).length == self.length
-  
-    end
-  
-  
-  
-    def my_any?(&block)
-  
-      unless block_given?
-  
-        block= lambda {|x| x}
-  
-      end
-  
-      return my_select(&block).length > 0
-  
-    end
-  
-  
-  
-    def my_none?(&block)
-  
-      unless block_given?
-  
-        block= lambda {|x| x}
-  
-      end
-  
-      return my_select(&block).length == 0
-  
-    end
-  
-  
-  
-    def my_count(num=nil, &block)
-  
-      unless num || block_given?
-  
-        return self.length
-  
-      end
-  
-      if num
-  
-        return self.my_select{|i| i==num}.length
-  
-      elsif block_given?
-  
-        return self.my_select(&block).length
-  
-      end
-  
-    end
-  
-  
-  
-    def my_map(p=nil)
-  
-      result=[]
-  
-      for i in 0...self.length
-  
-        result.push(p.call(self[i])) unless p==nil
-  
-        if block_given? && p==nil
-  
-          result.push(yield self[i])
-  
-        elsif !block_given? && p==nil
-  
-          return self.to_enum
-  
-        end
-  
-      end
-  
-      return result
-  
-    end
-  
-  
-  
-    def my_inject(initial=nil)
-  
-      if initial==nil
-  
-        accumulator=self[i]
-  
-      else
-  
-        accumulator=initial
-  
-      end
-  
-      self.my_each do |value|
-  
-        accumulator=yield(accumulator,value)
-  
-      end
-  
-      return accumulator
-  
-    end
-  
-  
-  
-      def multiply_els
-  
-        return self.my_inject(1){|product, value| product*value}
-  
-      end
-  
+
+    self
+
   end
+
+
+
+  def my_each_with_index
+
+    i = 0
+
+    while i < self.length
+
+      yield self[i], i
+
+      i += 1
+
+    end
+
+    self
+
+  end
+
+
+
+  def my_select
+
+    new_arr = []
+
+    self.my_each { |a| new_arr.push a if (yield a) }
+
+    new_arr
+
+  end
+
+
+
+  def my_all?
+
+    pass_element = 0
+
+    self.my_each { |a| pass_element += 1 if (yield a) }
+
+    pass_element == self.length ? true : false
+
+  end
+
+
+
+  def my_any?
+
+    result = false
+
+    self.my_each do |a|
+
+      if (yield a)
+
+        result = true
+
+        break
+
+      end
+
+    end
+
+    result
+
+  end
+
+
+
+  def my_none?
+
+    result = true
+
+    self.my_each do |a|
+
+      if (yield a)
+
+        result = false
+
+        break
+
+      end
+
+    end
+
+    result
+
+  end
+
+
+
+  def my_count
+
+    count = 0
+
+    self.my_each { |a| count += 1 if (yield a) }
+
+    count
+
+  end
+
+
+
+  def my_map
+
+    new_arr = []
+
+    self.my_each do |a|
+
+      new_element= (yield a)
+
+      new_arr << new_element
+
+    end
+
+    new_arr
+
+  end
+
+
+
+  def my_inject(num = nil)
+
+    if num.nil?
+
+      result = self[0]
+
+      index = 1
+
+      while index < self.length
+
+        result = (yield result, self[index])
+
+        index += 1
+
+      end
+
+      result
+
+    else
+
+      result = num
+
+      self.my_each { |a| result = (yield result, a) }
+
+      result
+
+    end
+
+  end
+
+
+
+  def my_map_proc (&code_block)
+
+    new_arr=[]
+
+    self.my_each do |a|
+
+      new_element= (code_block.call a)
+
+      new_arr << new_element
+
+    end
+
+    new_arr
+
+  end
+
+
+
+  def my_map_proc_block (&code_block)
+
+    new_arr = []
+
+    self.my_each do |a|
+
+      new_element = code_block ? (code_block.call a) : (yield a)
+
+      new_arr << new_element
+
+    end
+
+    new_arr
+
+  end
+
+
+
+end
+
+
+
+def multiply_els(arr)  ##raise error
+
+  arr.my_inject {|a,b| a*b}
+
+end
